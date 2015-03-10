18fab639922e4bc34de2275d3f560684a804d1cf
Gilles De Mey <gilles.de.mey@gmail.com>
2015-03-10 01:35:26 +0100

    Refactored code and added correct response delay metrics (and other metrics)

--- index.js
+++ index.js
@@ -1,78 +1,22 @@
 'use strict';
 
-var _         = require('lodash-node');
-var WebSocket = require('ws');
-
-var bunyan    = require('bunyan');
-var log;
-
 var Metrics = require('./metrics');
 
-function Cerberus(options) {
+function Cerberus() {}
+
+Cerberus.prototype.Metrics = function(opts) {
   var self = this;
 
-  // defaults
-  this.gatewayUrl = 'ws://localhost:8081';
-  this.debug      = false;
+  self._metrics = new Metrics(self, opts);
 
-  _.assign(this, options);
-
-  // Create Bunyan logger
-  this.log = log = bunyan.createLogger({
-    name  : 'cerberus-middleware',
-    level : self.debug ? bunyan.DEBUG : bunyan.INFO
-  });
-
-  // Cerberus sub-modules
-  this.Metrics = new Metrics(this).handler;
-
-  // Create websocket connection to Cerberus
-  var ws = this.ws = new WebSocket(this.gatewayUrl);
-
-  // Error handler
-  ws.on('error', self.onError);
-
-  self.openConnection();
-}
-
-/**
- * Add message handlers
- */
-Cerberus.prototype.openConnection = function() {
-
-  var ws = this.ws,
-    self = this;
-
-  if (!ws)
-    log.warn('No WebSocket connection initialized');
-
-  ws.on('open', function() {
-    log.debug('[Cerberus] Successfully connected');
-  });
-
-  ws.on('message', self.parseMessage);
-};
-
-/**
- * Parse message from WS connection
- */
-Cerberus.prototype.parseMessage = function(data, flags) {
-  data = JSON.parse(data);
-
-  if (data.type === 'WELCOME') {
-    log.debug('[Cerberus] Successfully authenticated');
-  }
+  // We're setting the context of the Metrics object to itself
+  // inside of the express middleware closure
+  return function(req, res, next) {
+    return self._metrics.handler.apply(self._metrics, arguments);
+  };
 
 };
 
-/**
- * /!\ Don't throw an error when something goes wrong!
- * We still want the API to work even if it's not aggregating data
- */
-Cerberus.prototype.onError = function(err) {
-  log.error(err);
-};
+var cerberus = new Cerberus();
 
-module.exports = function(options) {
-  return new Cerberus(options);
-};
+module.exports = cerberus;
--- metrics.js
+++ metrics.js
@@ -1,23 +1,85 @@
 'use strict';
 
 var _          = require('lodash-node');
+var WebSocket  = require('ws');
+var bunyan     = require('bunyan');
 var onFinished = require('on-finished');
 
-var self;
+var instance, log;
 
 function Metrics(client, options) {
+  var self = this;
+
   this.client = client;
 
+  // defaults
+  this.gatewayUrl = 'ws://localhost:8081';
+  this.debug      = false;
+
   _.assign(this, options);
+
+  self.metrics = {};
+
+  // Create Bunyan logger
+  self.log = log = bunyan.createLogger({
+    name  : 'Cerberus Metrics',
+    level : self.debug ? bunyan.DEBUG : bunyan.INFO,
+    serializers: bunyan.stdSerializers
+  });
+
+  // Create websocket connection to Cerberus
+  var ws = this.ws = new WebSocket(this.gatewayUrl);
+
+  // Error handler
+  ws.on('error', self.onError);
+
+  self.openConnection();
 }
+
+/**
+ * Add message handlers
+ */
+Metrics.prototype.openConnection = function() {
+
+  var ws = this.ws;
+
+  if (!ws)
+    log.warn('No WebSocket connection initialized');
+
+  ws.on('open', function() {
+    log.debug('Successfully connected');
+  });
+
+  ws.on('message', this.parseMessage);
+};
+
+/**
+ * Parse message from WS connection
+ */
+Metrics.prototype.parseMessage = function(data, flags) {
+  data = JSON.parse(data);
+
+  if (data.type === 'WELCOME') {
+    log.debug('Successfully authenticated');
+  }
+
+};
+
+/**
+ * /!\ Don't throw an error when something goes wrong!
+ * We still want the API to work even if it's not aggregating data
+ */
+Metrics.prototype.onError = function(err) {
+  log.error(err);
+};
 
 /**
  * Calculates hrtime difference between the start and end of a request.
  * Both in nanoseconds and milliseconds
  */
-Metrics.prototype.responseTime = function(req) {
+Metrics.prototype.responseTime = function(hrtime) {
 
-  var diff = process.hrtime(req._startAt);
+  var diff = process.hrtime(hrtime);
 
   var ns   = diff[0] * 1e9 + diff[1];
   var ms   = diff[0] * 1e3 + diff[1] * 1e-6;
@@ -34,21 +96,80 @@ Metrics.prototype.responseTime = function(req) {
  */
 Metrics.prototype.handler = function(req, res, next) {
 
+  var self = this;
+
+  var metrics = self.parseRequest(req);
+
   // If we're not using Morgan, add the timings ourself
   if (!req.hasOwnProperty('_startAt'))
     req._startAt = process.hrtime();
 
   // Wait for Express to send the response back to the client
   onFinished(res, function(err, res) {
-    var delay = self.responseTime(req);
-    self.client.log.info('delay: %d%s', delay.ms, 'ms');
+
+    if (err)
+      return log.error(err);
+
+    // add response data to metrics payload
+    metrics = _.assign(metrics, self.parseResponse(res));
+
+    self.sendMetrics(metrics);
   });
 
   next();
 };
 
-module.exports = function(client, options) {
-  options = options || {};
-  self = new Metrics(client, options);
-  return self;
+/**
+ * Capture incoming request data metrics
+ */
+Metrics.prototype.parseRequest = function(req) {
+
+  var self = instance;
+
+  if (!self.hostname)
+    self.hostname = req.hostname;
+
+  var delay = instance.responseTime(req._startAt);
+
+  return {
+    '_meta' : {
+      'hostname' : self.hostname
+    },
+    'request': {
+      'delay' : delay,
+      'href'  : req.protocol + '://' + self.hostname + req.path,
+      'path'  : req.path,
+    }
+  };
+
+};
+
+/**
+ * Capture response data metrics
+ */
+Metrics.prototype.parseResponse = function(res) {
+
+  var delay = instance.responseTime(res.req._startAt);
+
+  return {
+    'response': {
+      'contentLength' : res._headers['content-length'],
+      'delay'         : delay,
+      'statusCode'    : res.statusCode,
+    }
+  };
+
+};
+
+Metrics.prototype.sendMetrics = function(metrics) {
+  log.debug('response code was %s', metrics.statusCode);
+  if (metrics.contentLength)
+    log.debug('Payload size was %s bytes', metrics.contentLength);
+  log.debug('delay: %d%s', metrics.response.delay.ms, 'ms');
+  log.debug('Will report %j to %s', metrics, metrics.path);
+};
+
+module.exports = function(client, opts) {
+  instance = new Metrics(client, opts);
+  return instance;
 };
